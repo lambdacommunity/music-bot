c0daf3ab7147499250600dd9421fa3b4ae9bbfac
Simo Aleksandrov <simo3003@me.com>
2021-10-17 01:10:39 +0300

    feat: basic streaming from youtube

--- cmd/music-bot/main.go
+++ cmd/music-bot/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"github.com/diamondburned/arikawa/v3/voice"
 	"log"
 	"os"
+	"os/exec"
+	"time"
+
+	"github.com/diamondburned/arikawa/v3/voice"
+	"github.com/diamondburned/arikawa/v3/voice/voicegateway"
+	"github.com/diamondburned/oggreader"
+	"github.com/kkdai/youtube/v2"
+	"github.com/pkg/errors"
 
 	"github.com/diamondburned/arikawa/v3/gateway"
 	"github.com/diamondburned/arikawa/v3/utils/bot"
@@ -15,7 +22,9 @@ func main() {
 		log.Fatal("error loading .env file")
 	}
 
-	b := &Bot{}
+	b := &Bot{
+		YoutubeClient: youtube.Client{},
+	}
 
 	bot.Run(os.Getenv("DISCORD_TOKEN"), b,
 		func(ctx *bot.Context) error {
@@ -27,10 +36,54 @@ func main() {
 }
 
 type Bot struct {
-	Ctx *bot.Context
+	Ctx           *bot.Context
+	YoutubeClient youtube.Client
 }
 
 func (b *Bot) Play(e *gateway.MessageCreateEvent) error {
+	video, err := b.YoutubeClient.GetVideo(e.Message.Content)
+	if err != nil {
+		return err
+	}
+
+	formats := video.Formats.WithAudioChannels() // only get videos with audio
+	stream, _, err := b.YoutubeClient.GetStream(video, &formats[0])
+	if err != nil {
+		return err
+	}
+
+	ffmpeg := exec.Command(
+		"ffmpeg",
+		// Streaming is slow, so a single thread is all we need.
+		"-hide_banner", "-threads", "1", "-loglevel", "error",
+		// Input file. This should be changed.
+		"-i", "-",
+		// Output format; leave as "libopus".
+		"-c:a", "libopus",
+		// Bitrate in kilobits. This doesn't matter, but I recommend 96k as the
+		// sweet spot.
+		"-b:a", "96k",
+		// Frame duration should be the same as what's given into
+		// udp.ResetFrequency.
+		"-frame_duration", "60",
+		// Disable variable bitrate to keep packet sizes consistent. This is
+		// optional, but it technically reduces stuttering.
+		"-vbr", "off",
+		// Output format, which is opus, so we need to unwrap the opus file.
+		"-f", "opus", "-",
+	)
+	ffmpeg.Stdin = stream
+	ffmpeg.Stderr = os.Stderr
+
+	stdout, err := ffmpeg.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	err = ffmpeg.Start()
+	if err != nil {
+		return err
+	}
+
 	session, err := voice.NewSession(b.Ctx.State)
 	if err != nil {
 		return err
@@ -41,15 +94,28 @@ func (b *Bot) Play(e *gateway.MessageCreateEvent) error {
 		return err
 	}
 
-	userChannelId := voiceState.ChannelID
-
-	err = session.JoinChannel(e.GuildID, userChannelId, false, true)
+	userChannelID := voiceState.ChannelID
+	err = session.JoinChannel(e.GuildID, userChannelID, false, true)
 	if err != nil {
 		return err
 	}
 	defer session.Leave()
 
-	select {}
+	udp := session.VoiceUDPConn()
+	udp.ResetFrequency(60*time.Millisecond, 2880)
+
+	if err := session.Speaking(voicegateway.Microphone); err != nil {
+		return errors.Wrap(err, "failed to send speaking")
+	}
+
+	if err := oggreader.DecodeBuffered(udp, stdout); err != nil {
+		return errors.Wrap(err, "failed to decode ogg")
+	}
+
+	if err := ffmpeg.Wait(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -8,10 +8,16 @@ require (
 )
 
 require (
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/diamondburned/oggreader v0.0.0-20201118014549-87df9534b647 // indirect
+	github.com/faiface/beep v1.1.0 // indirect
 	github.com/gorilla/schema v1.2.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.2 // indirect
+	github.com/kkdai/youtube/v2 v2.7.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
-	golang.org/x/sys v0.0.0-20201018230417-eeed37f84f13 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
 )
