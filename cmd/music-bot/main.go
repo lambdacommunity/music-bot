c0dae42969e9b6aa20c006af3ea0a963be1c5b84
Simo Aleksandrov <simo3003@me.com>
2021-10-16 22:47:38 +0300

    feat: add godotenv + .env.example

--- cmd/music-bot/main.go
+++ cmd/music-bot/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/diamondburned/arikawa/v3/gateway"
 	"github.com/diamondburned/arikawa/v3/utils/bot"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	bot.Run(os.Getenv("DISCORD_TOKEN"), &Bot{},
 		func(ctx *bot.Context) error {
 			ctx.HasPrefix = bot.NewPrefix("!")
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/diamondburned/arikawa/v3 v3.0.0-rc.2
 require (
 	github.com/gorilla/schema v1.2.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
 )
